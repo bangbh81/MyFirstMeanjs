636802c681de9f2b5ebfbc0de587e8070842ee56
peter <peter@wiznet.co.kr>
2016-10-05 17:42:40 +0900

    error correct

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -9,14 +9,14 @@
   function HomeController($scope) {
     var vm = this;
     $scope.click = function(event) {
-            docClient.get(params, function(err, data) {
-              if (err) {
-                console.error('Unable to read item. Error JSON:', JSON.stringify(err, null, 2));
-              } else {
-                console.log('GetItem succeeded:', JSON.stringify(data, null, 2));
-              }
-            });
+      docClient.get(params, function(err, data) {
+        if (err) {
+          console.error('Unable to read item. Error JSON:', JSON.stringify(err, null, 2));
+        } else {
+          console.log('GetItem succeeded:', JSON.stringify(data, null, 2));
         }
+      });
+    };
   }
   AWS.config.update({
     region: 'ap-northeast-1',
